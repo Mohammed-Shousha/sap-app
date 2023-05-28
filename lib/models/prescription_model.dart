6ba2df2f9e116ce86f4fb5f01a5ac7c55fbe1c87
Mohammed Shousha <mshousha29@gmail.com>
2023-05-28 22:11:48 +0300

    adding prescriptions provider

--- lib/models/prescription_model.dart
+++ lib/models/prescription_model.dart
@@ -27,6 +27,7 @@ class PrescriptionModel {
       isReceived: json['isReceived'],
       medicines: ((json['medicines'] ?? []) as List<dynamic>)
           .map((medicine) => PrescriptionMedicine(
+                id: medicine['medicineId'],
                 name: medicine['medicineName'],
                 quantity: medicine['quantity'],
                 price: medicine['price'],
@@ -38,15 +39,37 @@ class PrescriptionModel {
 }
 
 class PrescriptionMedicine {
+  final String id;
   final String name;
   final int quantity;
   final num price;
   final String doctorInstructions;
 
   PrescriptionMedicine({
+    required this.id,
     required this.name,
     required this.price,
     required this.quantity,
     required this.doctorInstructions,
   });
+
+  factory PrescriptionMedicine.fromJson(Map<String, dynamic> json) {
+    return PrescriptionMedicine(
+      id: json['medicineId'],
+      name: json['medicineName'],
+      price: json['price'],
+      quantity: int.parse(json['quantity']),
+      doctorInstructions: json['doctorInstructions'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'medicineId': id,
+      'medicineName': name,
+      'price': price,
+      'quantity': quantity,
+      'doctorInstructions': doctorInstructions,
+    };
+  }
 }
---
+++ lib/providers/prescriptions_provider.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:sap/models/prescription_model.dart';
+import 'package:sap/utils/graphql_mutations.dart';
+import 'package:sap/utils/graphql_queries.dart';
+
+class PrescriptionsProvider extends ChangeNotifier {
+  final GraphQLClient client;
+
+  PrescriptionsProvider({
+    required this.client,
+  });
+
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  String _errorMessage = '';
+  String get errorMessage => _errorMessage;
+
+  List<PrescriptionModel> _prescriptions = [];
+  List<PrescriptionModel> get prescriptions => _prescriptions;
+
+  PrescriptionModel? _prescription;
+  PrescriptionModel? get prescription => _prescription;
+
+  num get prescriptionTotal => _prescription!.medicines!
+      .map((medicine) => medicine.price)
+      .reduce((value, element) => value + element);
+
+  Future<void> getUserPrescriptions(String userId) async {
+    _isLoading = true;
+    _errorMessage = '';
+    notifyListeners();
+
+    final result = await client.query(
+      QueryOptions(
+        document: gql(
+          GraphQLQueries.getUserPrescriptions,
+        ),
+        variables: {
+          'userId': userId,
+        },
+        fetchPolicy: FetchPolicy.noCache,
+      ),
+    );
+
+    if (result.hasException) {
+      _isLoading = false;
+      _errorMessage = 'Error fetching prescriptions';
+    } else {
+      _isLoading = false;
+      _prescriptions = (result.data!['prescriptionsByUser'] as List)
+          .map((prescription) => PrescriptionModel.fromJson(prescription))
+          .toList();
+    }
+
+    notifyListeners();
+  }
+
+  Future<void> getPrescriptionDetails(prescriptionId) async {
+    _isLoading = true;
+    _errorMessage = '';
+
+    final result = await client.query(
+      QueryOptions(
+        document: gql(
+          GraphQLQueries.getPrescriptionDetails,
+        ),
+        variables: {
+          'id': prescriptionId,
+        },
+        fetchPolicy: FetchPolicy.noCache,
+      ),
+    );
+
+    if (result.hasException) {
+      _isLoading = false;
+      _errorMessage = 'Error fetching prescription details';
+    } else {
+      _isLoading = false;
+      _prescription =
+          PrescriptionModel.fromJson(result.data!['prescriptionById']);
+    }
+
+    notifyListeners();
+  }
+
+  Future<void> addPrescription(
+    String patientId,
+    String doctorId,
+    List<PrescriptionMedicine> medicines,
+  ) async {
+    _isLoading = true;
+    _errorMessage = '';
+    notifyListeners();
+
+    final result = await client.mutate(
+      MutationOptions(
+        document: gql(
+          GraphQLMutations.addPrescription,
+        ),
+        variables: {
+          'patientId': patientId,
+          'doctorId': doctorId,
+          'medicines': medicines.map((medicine) => medicine.toJson()).toList(),
+        },
+      ),
+    );
+
+    if (result.hasException) {
+      _errorMessage = result.exception!.graphqlErrors.first.message;
+    }
+
+    _isLoading = false;
+
+    notifyListeners();
+  }
+}
